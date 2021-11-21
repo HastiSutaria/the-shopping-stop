a8620177444ad5e91e108ebebe74f35161dca5d6
HastiSutaria <hastisutaria.ict19@gmail.com>
2021-11-21 12:56:20 +0530

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBkWBYgEsYyc7SwLEMlAAFCd1Mp1uPwEko",
+    authDomain: "shoppingstop-4d309.firebaseapp.com",
+    projectId: "shoppingstop-4d309",
+    storageBucket: "shoppingstop-4d309.appspot.com",
+    messagingSenderId: "1001834739072",
+    appId: "1:1001834739072:web:0daeed1605602e0ce3bd65",
+    measurementId: "G-6CX92K94D1"
+  };
