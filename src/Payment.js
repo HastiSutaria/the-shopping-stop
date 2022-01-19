65ad1c791c1e9fe5a7a97f7528a075c4afce9a94
HastiSutaria <hastisutaria.ict19@gmail.com>
2022-01-19 22:36:01 +0530

    payment commit

--- src/Payment.js
+++ src/Payment.js
@@ -1,7 +1,54 @@
 import React from "react";
 import "./Payment.css";
+import CheckoutProduct from "./CheckoutProduct";
+import { Link } from "react-router-dom"
+import { useStateValue } from "./StateProvider";
+
 function Payment() {
-  return <div className="payment"></div>;
+  const [{ basket, user }, dispatch] = useStateValue();
+  return (
+    <div className="payment">
+      <div className="payment__container">
+        <h1>
+          Checkout {<Link to="/checkout">({basket?.length} items)</Link>}
+        </h1>
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Delivery Address</h3>
+          </div>
+          <div className="payment__address">
+            <p>{user?.email}</p>
+            <p>123 React Lane</p>
+            <p>Los Angles, CA</p>
+          </div>
+        </div>
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Review items and delivery</h3>
+          </div>
+          <div className="payment__items">
+            {basket.map((items) => (
+              <CheckoutProduct
+                id={items.id}
+                title={items.title}
+                image={items.image}
+                price={items.price}
+                rating={items.rating}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="payment__section">
+        <div className="payment__title">
+            <h3>Payment Method</h3>
+          </div>
+          <div className="payment__details">
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Payment;
